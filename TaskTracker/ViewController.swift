3d89d7c96ad51b9a951559937ec5c074f029bc53
Evgeny Lipadat <evgeniy.lipadat@simbirsoft.com>
2019-05-20 19:27:29 +0300

    Initial Commit

---
+++ TaskTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskTracker
+//
+//  Created by Evgeny Lipadat on 20/05/2019.
+//  Copyright © 2019 Evgeny Lipadat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
